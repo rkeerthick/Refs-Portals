3828ed267d2a8e2e45d9de44a15f2dba5aefd2d7
Divum <rkeerthick1@gmail.com>
2023-10-27 15:43:13 +0530

    first commit

--- src/components/TimerChallege.jsx
+++ src/components/TimerChallege.jsx
@@ -4,33 +4,38 @@ import ResultModal from "./ResultModal";
 const TimerChallege = ({ title, targetTime }) => {
   const timer = useRef();
   const dialog = useRef();
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const isTimerActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.showModal();
+    setTimeRemaining(targetTime * 1000);
+  }
   const handleStart = () => {
     setTimerStarted(true);
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.showModal();
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   };
   const handleStop = () => {
-    clearTimeout(timer.current);
+    dialog.current.showModal();
+    clearInterval(timer.current);
   };
   return (
     <>
       <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You lost</p>}
+        {!isTimerActive && <p>You lost</p>}
         <p className="challenge-item">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={isTimerActive ? handleStop : handleStart}>
+            {isTimerActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p>{timerStarted ? "Timer is running..." : "Timer inactive"}</p>
+        <p>{isTimerActive ? "Timer is running..." : "Timer inactive"}</p>
       </section>
     </>
   );
